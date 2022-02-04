1790354a29d6e3e491425e615e9c85fe8da40ed8
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-04 17:56:34 -0500

    Implement channel chat toggle

--- src/Components/Channel/Channel.js
+++ src/Components/Channel/Channel.js
@@ -1,11 +1,19 @@
 import "../../CSS/Channel.css";
-import React from "react";
-  
+import { BiMessageAltDetail } from "react-icons/bi";
+import React, {useState} from "react";
+
 const Channel = () => {
+  const [view, setView] = useState(true);
+  
+  const toggleChannelView = (e) => {
+    setView(!view);
+  }
+
   return (
     <>
-      <div id="channel">
+      <div className={`channel ${view ? "" : "hidden"}`}>
         <div id="submit-wrapper">
+          <BiMessageAltDetail id="toggle-chat" onClick={(e) => {toggleChannelView(e)}}/>
           <div id="input"><input type="text"/></div>
           <div id="submit">Submit</div>
         </div>
