a675131d8457f6f1fa2ec56c573d45db45176ea7
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-04 22:11:11 -0500

    Add placeholder text to message input

--- src/Components/Channel/Channel.js
+++ src/Components/Channel/Channel.js
@@ -14,7 +14,7 @@ const Channel = () => {
       <div className={`channel ${view ? "" : "hidden"}`}>
         <div id="submit-wrapper">
           <BiMessageAltDetail id="toggle-chat" onClick={(e) => {toggleChannelView(e)}}/>
-          <div id="input"><input type="text"/></div>
+          <div id="input"><input type="text" placeholder="Enter message"/></div>
           <div id="submit">Submit</div>
         </div>
         <div id="messages">
