a6fb4e2a3dde9df6e49692f86da4d230e7ef587b
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-09 18:47:42 -0500

    Prepare for Heroku Deployment

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+import express from "express";
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
