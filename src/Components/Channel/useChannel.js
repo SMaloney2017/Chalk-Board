34ccde9ae5197a77901fb76ca216696555358ea9
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-09 18:24:13 -0500

    Prepare for Heroku Deployment

--- src/Components/Channel/useChannel.js
+++ src/Components/Channel/useChannel.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import socketIOClient from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newMessageEvent";
-const SOCKET_SERVER_URL = "http://localhost:4000";
+const SOCKET_SERVER_URL = "/";
 
 const useChannel = (id) => {
   const [messages, setMessages] = useState([]);
