4bf79ff9f97121768ea5afd849405f49ea82ebef
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-02-09 19:50:22 -0500

    Prepare for Heroku Deployment

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ const NEW_UNDO_EVENT = "undoLineEvent";
 const NEW_REDO_EVENT = "redoLineEvent";
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./build")));
+  app.use(express.static(path.join(__dirname, "build")));
 };
 
 io.on("connection", (socket) => {
@@ -45,6 +45,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     socket.leave(id);
   });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build"));
 });
 
 server.listen(PORT, () => {
